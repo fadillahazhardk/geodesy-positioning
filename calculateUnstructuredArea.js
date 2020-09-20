6433e7c09a97af2625f90051d3659ca4e55de95e
fadillahazhardk <fadillahazhardk@gmail.com>
2020-09-20 11:06:06 +0700

    add triangle method

--- calculateUnstructuredArea.js
+++ calculateUnstructuredArea.js
@@ -1,37 +1,83 @@
 const convert = require("convert-units");
 
-//Insert your trapesium or triangle data in cm(triangle is trapesium with a or b value is zero)
-const areasInCm = [
-  { a: 12, b: 12, height: 2 },
-  { a: 4, b: 4, height: 2 },
+//Insert your trapesium data
+const trapesiumDataAreasInCm = [
+  { a: 0, b: 0.6, height: 0.1 },
+  { a: 0.6, b: 1.7, height: 14.1 },
+  { a: 1.7, b: 2.15, height: 0.1 },
+  { a: 2.15, b: 2.4, height: 4.5 },
+  { a: 2.4, b: 1.35, height: 1.5 },
+  { a: 1.35, b: 0.75, height: 1.1 },
+  { a: 0.75, b: 0, height: 1.7 },
+  { a: 1.15, b: 0, height: 2.2 },
+  { a: 2.5, b: 1.15, height: 15.7 },
+  { a: 0, b: 2.5, height: 5.4 },
 ];
 
-convertAreaFromCmToM = (area) => {
-  return convert(area).from("cm2").to("m2");
+const triangleDataAreasInCm = [
+  { a: 5.95, b: 0.65, c: 6.05 },
+  { a: 6.05, b: 14.2, c: 9.8 },
+  { a: 9.8, b: 15.75, c: 7.35 },
+  { a: 7.35, b: 7.5, c: 0.5 },
+  { a: 7.5, b: 4.75, c: 4.05 },
+  { a: 4.05, b: 2.55, c: 1.7 },
+  { a: 2.55, b: 1.25, c: 2 },
+  { a: 2, b: 1.9, c: 2.55 },
+];
+
+calculateTrapesiumInCmWithScale = (obj, scale) => {
+  const a = obj.a * scale;
+  const b = obj.b * scale;
+  const height = obj.height * scale;
+  return ((a + b) * height) / 2;
 };
-convertAreaFromCmToHa = (area) => {
-  return convert(area).from("cm2").to("ha");
+calculateTriangleInCmWithScale = (obj, scale) => {
+  const a = obj.a * scale;
+  const b = obj.b * scale;
+  const c = obj.c * scale;
+  const s = (a + b + c) / 2;
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 };
-calculateTrapesium = (obj) => {
-  return ((obj.a + obj.b) * obj.height) / 2;
-};
-calculateAreaFromCm = (arr, unit = "cm2") => {
+calculateTrapesiumAreaInCm = (arr, scale) => {
   let area = null;
   arr.forEach((obj) => {
-    switch (unit) {
-      case "m2":
-        area += convertAreaFromCmToM(calculateTrapesium(obj));
-        break;
-      case "ha":
-        area += convertAreaFromCmToHa(calculateTrapesium(obj));
-        break;
-      default:
-        area += calculateTrapesium(obj);
-        break;
-    }
+    area += calculateTrapesiumInCmWithScale(obj, scale);
   });
-  return `area is ${area} ${unit}`;
+  return area;
+};
+calculateTriangleAreaInCm = (arr, scale) => {
+  let area = null;
+  arr.forEach((obj) => {
+    area += calculateTriangleInCmWithScale(obj, scale);
+  });
+  return area;
 };
 
 //Calculate your area based on data you provide
-console.log(calculateAreaFromCm(areasInCm, "ha"));
+const triangleMethodToHa = convert(
+  calculateTriangleAreaInCm(triangleDataAreasInCm, 150)
+)
+  .from("cm2")
+  .to("ha");
+const trapesiumMethodToHa = convert(
+  calculateTrapesiumAreaInCm(trapesiumDataAreasInCm, 150)
+)
+  .from("cm2")
+  .to("ha");
+const triangleMethodToM2 = convert(
+  calculateTriangleAreaInCm(triangleDataAreasInCm, 150)
+)
+  .from("cm2")
+  .to("m2");
+const trapesiumMethodToM2 = convert(
+  calculateTrapesiumAreaInCm(trapesiumDataAreasInCm, 150)
+)
+  .from("cm2")
+  .to("m2");
+console.log("with triangle: " + triangleMethodToHa + " ha");
+console.log("with triangle: " + triangleMethodToM2 + " m2");
+
+console.log("with trapesium: " + trapesiumMethodToHa + " ha");
+console.log("with trapesium: " + trapesiumMethodToM2 + " m2");
+
+
